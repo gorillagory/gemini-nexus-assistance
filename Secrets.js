c3e4b3a59f15a1841338ff8f0f506d1e2559241a
gorillagory <51069873+gorillagory@users.noreply.github.com>
2026-01-11 18:12:39 +0800

    Stop tracking Secrets.js

--- Secrets.js
+++
@@ -1,8 +0,0 @@
-// ==========================================
-// 🔒 SECRETS VAULT
-// This file is ignored by Git. Do not share.
-// ==========================================
-
-const SECRETS = {
-  GEMINI_API_KEY: 'AIzaSyCxqdgzEyDAuWM6XwnbxYzYpDCCxnTklzI'
-};
